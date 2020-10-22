0a98b601ba475fe08dd22b6ad7356adb72d5eed2
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2020-10-22 14:22:36 -0700

    removes db again

--- db/migrate/20201020183329_createuser.rb
+++
@@ -1,8 +0,0 @@
-class Createuser < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.index :messages
-    end
-  end
-end
--- db/migrate/20201020205029_createmessage.rb
+++
@@ -1,8 +0,0 @@
-class Createmessage < ActiveRecord::Migration[6.0]
-  def change
-    create_table :messages do |t|
-      t.text :user_id
-      t.text :joke_id
-    end
-  end
-end
